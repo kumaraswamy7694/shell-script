5021de010eeadda875e22d1f90575f067489151a
saik90305@gmail.com <saik90305@gmail.com>
2024-11-19 14:04:30 +0530

    changes

--- 06-array.sh
+++ 06-array.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PERSONS=("Ramesh" "suresh" "saikumar")
 # INSIDE ARRAY WE ARE ALWAYS REFER VALUE WITH INDEX AND IT STARTS FORM 0
-echo "first person is : ${PERSONS[01]}"
+echo "first person is : ${PERSONS[0]}"
