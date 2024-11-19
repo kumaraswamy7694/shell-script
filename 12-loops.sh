5327802fdc0c64ea8f7fea6cb819720e54bdb17c
saik90305@gmail.com <saik90305@gmail.com>
2024-11-19 21:53:43 +0530

    changes

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+N="\e[0m"
+if [$USERID -ne 0];
+then
+    echo "erro: $R please run this script with root access $N"
+    exit 1
+fi 
 
-for i in {1..20} # it will loop from 1 to 20
+for i in $@ # it will loop from 1 to 20
 do
-    echo $i
+    yum install $i -y
 done
