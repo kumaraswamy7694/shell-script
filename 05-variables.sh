1802e35859a9c4d21dc5e011939a08e37d3a1edb
saik90305@gmail.com <saik90305@gmail.com>
2024-11-19 13:36:25 +0530

    changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,8 +2,8 @@
 
  echo "please enter you username"
  read USERNAME # this is the variable where the value of the one uesr entered
- echo "username entered is :$USERNAME"
+ #echo "username entered is :$USERNAME"
  echo "please enter you password"
  read PASSWORD
- echo "password entered is :$PASSWORD"
- 
+ #echo "password entered is :$PASSWORD"
+ echo "username and password entered is :$USERNAME $PASSWORD "
