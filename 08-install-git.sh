7004faf7f985d2fccbfe71e7978aa1a70be29467
saik90305@gmail.com <saik90305@gmail.com>
2024-11-19 15:54:29 +0530

    changes

--- 08-install-git.sh
+++ 08-install-git.sh
@@ -6,3 +6,11 @@ then
 fi
 
 yum install mysql -y 
+yum install postfix -y
+if [$? -ne 0]
+then 
+    echo "installation of postfix is error"
+    exit 1
+else 
+    echo "installation of postfix is success"
+fi
