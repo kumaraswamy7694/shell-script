7b6041a23424c472fe6d413755041d987fe0da5a
saik90305@gmail.com <saik90305@gmail.com>
2024-11-19 13:45:20 +0530

    changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
- NAME=$1
+ NAME=KUMARSWAMY
  echo "hello $NAME, welcome to HYD"
+ echo " enter you age"
  read AGE
  echo "you are $AGE old and also eligible for voting"
  read PLACE
  echo "you are from $PLACE ,such a bueatyful city"
- 
