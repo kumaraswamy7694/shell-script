4c28653d927f4b23183e29474546c00b5da93355
saik90305@gmail.com <saik90305@gmail.com>
2024-11-19 13:46:23 +0530

    changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,5 +5,6 @@
  echo " enter you age"
  read AGE
  echo "you are $AGE old and also eligible for voting"
+ echo "enter you place of living"
  read PLACE
- echo "you are from $PLACE ,such a bueatyful city"
+ echo "you are from $PLACE ,such a bueatyful city in India"
