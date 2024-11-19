ec857160afb3e18b41187409e83eadd67eae70e8
saik90305@gmail.com <saik90305@gmail.com>
2024-11-19 13:15:12 +0530

    changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,9 +3,13 @@
 #declaring of varible
 PERSON1=$1
 PERSON2=$2
+DATE=$(date)
+CAL=$(cal)
 
 #referring variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am fine too. how is your work?"
 echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
+echo "date is $DATE"
+echo "cal is $CAL"
