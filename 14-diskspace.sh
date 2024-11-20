0c763c46c539cc11b1df2783217e5f3f95540695
saik90305@gmail.com <saik90305@gmail.com>
2024-11-20 21:02:41 +0530

    message

---
+++ 14-diskspace.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# colours
+# validation
+# log
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+DISK_USAGE=$(df -hT | grep -vE ' tmpfs|FileSystem ')
+DISK_USAGE_THERESHOLD=1
+
+# ifs= means internal field seperation
+while IFS= read line
+do 
+    echo "output :$line"
+done <<< $DISK_USAGE
+
+
