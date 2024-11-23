c851816ee886f68c58347332b28c038bfdc4f9ae
saik90305@gmail.com <saik90305@gmail.com>
2024-11-23 17:27:45 +0530

    added ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAMES=("mongodb redis" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-0b4f379183e5706b9
 SECURITY_GROUP_ID=sg-0729448b7fb37ace1
@@ -8,7 +8,7 @@ SECURITY_GROUP_ID=sg-0729448b7fb37ace1
 # if mysql or mongodb instance_type should be t3.medium for all it is t2.micro
 for i in "${NAMES[@]}"
 do
-    if [[ $i == "mongodb" || $i == "mysql"]];
+    if [[ "$i" == "mongodb" || "$i" == "mysql"]];
     then 
         INSTANCE_TYPE="t3.medium"
     else
