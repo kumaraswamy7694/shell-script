b5e7a9c69ffce342b598cef4e5dcdb6142b40cdc
saik90305@gmail.com <saik90305@gmail.com>
2024-11-23 16:56:54 +0530

    added ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,5 +3,5 @@
 NAMES=("mongodb redis" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 for i in "${NAMES[@]}"
 do
-    echo "NAME: $1"
+    echo "NAME: $i"
 done
