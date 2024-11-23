70882dc00e2253b7d286c576b99eef7a13505ddd
saik90305@gmail.com <saik90305@gmail.com>
2024-11-23 18:04:50 +0530

    added ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -5,6 +5,7 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"
 IMAGE_ID=ami-0b4f379183e5706b9
 SECURITY_GROUP_ID=sg-0729448b7fb37ace1
 INSTANCE_TYPE=""
+DOMAIN_NAME=kumaraswamy.xyz
 
 # Loop through each instance name
 for i in "${NAMES[@]}"
@@ -23,5 +24,20 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
   
     echo "created $i instance: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z011735029B6AONSQ3Z1E --change-batch '
+{
+            "Comment": "CREATE/DELETE/UPSERT a record ",
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "'$i.$DOMAIN_NAME'",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                 "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+}}]
+}
+'
+
 
 done
