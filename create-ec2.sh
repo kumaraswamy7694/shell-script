e9b8b655974782ad83ae98896d18105d33672cb3
saik90305@gmail.com <saik90305@gmail.com>
2024-11-23 17:21:42 +0530

    added ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -8,7 +8,7 @@ SECURITY_GROUP_ID=sg-0729448b7fb37ace1
 # if mysql or mongodb instance_type should be t3.medium for all it is t2.micro
 for i in "${NAMES[@]}"
 do
-    if [[ $i == "mongodb" || $i == "mysql"]]
+    if [[ $i == "mongodb" || $i == "mysql"]];
     then 
         INSTANCE_TYPE="t3.medium"
     else
