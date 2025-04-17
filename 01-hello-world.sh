76c07f8a41d743098e80b2e229b5ecccd284cc57
saik90305@gmail.com <saik90305@gmail.com>
2025-04-17 15:39:23 +0530

    added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -6,3 +6,4 @@ PERSON2=Suresh
 echo "Ramesh: Hi $PERSON1 , good morning"
 echo "suresh: hi $PERSON2 , iam good . How are you "
 echo " neeku enduku ra  $PERSON1 , vinava $PERSON2 " 
+echo " chala chusam ra $PERSON1 , nenu $PERSON2 ni chudalenu intavaraku ela "
