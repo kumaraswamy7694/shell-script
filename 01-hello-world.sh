69155f3008bb95c066f7131b747a0d9e68ee8d6d
saik90305@gmail.com <saik90305@gmail.com>
2025-04-17 15:44:55 +0530

    added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "Hello, I am learning DevOps"
-PERSON1=Ramesh
-PERSON2=Suresh
-echo "Ramesh: Hi $PERSON1 , good morning"
-echo "suresh: hi $PERSON2 , iam good . How are you "
-echo " neeku enduku ra  $PERSON1 , vinava $PERSON2 " 
-echo " chala chusam ra $PERSON1 , nenu $PERSON2 ni chudalenu intavaraku ela "
+echo " Hello HYDERABAD ela unnaru ?"
+NAME1=SAIKUMAR
+NAME2=KUMAR
+echo " meeru $NAME1 EPPUDU OCCHARU AND $NAME2 NEEKU SIGGU UNDA RA ASSALU"
+
+
