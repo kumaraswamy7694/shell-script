a84c275670053ecc904cd728e42ebb34dcc7b8bd
saik90305@gmail.com <saik90305@gmail.com>
2025-04-18 12:20:33 +0530

    added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-echo "Hello, I am learning DevOps"
-PERSON1=Ramesh
-PERSON2=Suresh
-echo "Ramesh: Hi $PERSON1 , good morning"
-echo "suresh: hi $PERSON2 , iam good . How are you "
-echo " neeku enduku ra  $PERSON1 , vinava $PERSON2 " 
-echo " chala chusam ra $PERSON1 , nenu $PERSON2 ni chudalenu intavaraku ela "
-echo "hello"
+#shebang you should always use #!/bin/bash how to execute the script
+echo "hi hello world , iam learning devops "
