c7636d07ea3af476707bb1f6ad6a1899bf99896c
saik90305@gmail.com <saik90305@gmail.com>
2025-04-18 12:30:18 +0530

    added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 #shebang you should always use #!/bin/bash how to execute the script
-echo "hi hello world , iam learning devops "
-echo "script added"
-US1=kumar
-US2=sai
-echo "nuvvu $US1 and $US2 ela unnadu"
-
-US3=vamshi
-US4=vinod
-echo "you entered amount is 5000 mr $US3 in that give amount to $US4"
-    
+P1=person1
+P2=person2
+echo "hello mister $P1 and neeku siggu unda ra asslu "
+echo "picchoda mr $P2"
+   
