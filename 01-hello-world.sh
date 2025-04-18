511cc78854605677d0febad8b1f5862354a2d6fc
saik90305@gmail.com <saik90305@gmail.com>
2025-04-18 12:39:30 +0530

    added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #shebang you should always use #!/bin/bash how to execute the script
-P1=person1
-P2=person2
+P1=sai
+P2=kumar
 echo "hello mister $P1 and neeku siggu unda ra asslu "
 echo "picchoda mr $P2"
    
