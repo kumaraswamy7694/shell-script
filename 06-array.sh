15bd96b51e3ff9ef2f33f7379361954c3c8a7c6a
saik90305@gmail.com <saik90305@gmail.com>
2025-04-18 12:55:24 +0530

    added

--- 06-array.sh
+++ 06-array.sh
@@ -3,3 +3,8 @@ PERSONS=("Ramesh" "suresh" "saikumar")
 # INSIDE ARRAY WE ARE ALWAYS REFER VALUE WITH INDEX AND IT STARTS FORM 0
 echo "first person is : ${PERSONS[0]}"
 echo "second person is : ${PERSONS[1]}"
+echo "third vaadu evadu antey : ${PERSONS[2]}"
+ 
+
+
+ 
