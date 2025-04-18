002d3e09f0745c8af96d36bd362b367e5942359c
saik90305@gmail.com <saik90305@gmail.com>
2025-04-18 12:53:20 +0530

    added

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,7 +4,7 @@ echo " please enter you username"
 read  USERNME 
 echo "username entered is $USERNAME"
 echo "please enter your password"
-read  PASSWORD
+read -s PASSWORD
 echo " password entered is : $PASSWORD"
 # read command will run until enter key is pressed.
 
