86c0c36d9c1dc4ce2ad3dcc0c94072b0ae8ab57f
saik90305@gmail.com <saik90305@gmail.com>
2025-04-18 12:42:44 +0530

    added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shell will run the cmd inside $() and stores output in varible called DATE
 DATE=$(date)
-TIME=$(time)
+TIME=$(cal)
 echo "hello, time is : $DATE" 
 echo "neeku time occhindi ra adi endnate $TIME"
    
