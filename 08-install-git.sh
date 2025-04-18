cadcbab933e26a3c3739389a24c1c6574a5c6083
saik90305@gmail.com <saik90305@gmail.com>
2025-04-18 15:08:32 +0530

    added

--- 08-install-git.sh
+++ 08-install-git.sh
@@ -7,6 +7,11 @@ USERID=$(id -u)
 if [$USERID -ne 0]
 then
     echo "error :: please runt this script with root access"
+    exit 1
 fi
+
 yum install git -y
 git --version 
+#shell script by default will not stop will have exit status 
+#if $? -eq 0 previous command is success if not it is failure
+#
