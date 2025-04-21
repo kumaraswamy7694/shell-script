07ee76e6539847c1339676edf3846968de4ab30b
saik90305@gmail.com <saik90305@gmail.com>
2025-04-21 10:37:17 +0530

    added

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,35 +1,31 @@
-!/bin/bash
+#!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
 
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-        echo -e "$2...$R FAILURE $N"
+        echo "installation is failure"
         exit 1
     else
-        echo  -e "$2...$GSUCCESS $N "
+        echo  -e "iinstallation is success"
     fi
 }
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root access."
+    echo "error :: please run this script"
     exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
+#else
+#    echo "You are super user."
 fi
 
-dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL"
+yum install mysql -y 
 
-dnf install git -y &>>$LOGFILE
-VALIDATE $? "Installing Git"
+VALIDATE $?
+yum install postfix -y
+VALIDATE $?
+    
+    
 
