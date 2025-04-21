0a055d885288cc21df4d49b485c62de752627b49
saik90305@gmail.com <saik90305@gmail.com>
2025-04-21 10:57:45 +0530

    added

--- 10-special-vars.sh
+++ 10-special-vars.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 VAR1=$1
 VAR2=$2
-VAR3=$3
-VAR4=$4
 
-echo "variable 1:$VAR1 $VAR2 $VAR3 $VAR4"
-echo "all variable are: $@"
-echo "no of vars are: $#"
+echo "variable 1 : $VAR1"
+echo "all vars : $@"
