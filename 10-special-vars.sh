ba6b8e6311c0aaa066f33be6f8e425aa01a95c31
saik90305@gmail.com <saik90305@gmail.com>
2025-04-21 11:30:06 +0530

    added

--- 10-special-vars.sh
+++ 10-special-vars.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-VAR1=$1
-VAR2=$2
-echo "script name : $0"
-# $0 is the name of the script
-echo "variable 1 : $VAR1"
-echo "all vars : $@"
-echo " no of vars : $#"
-# how many varialbes passed
+# VAR1=$1
+# VAR2=$2
+# echo "script name : $0"
+# # $0 is the name of the script
+# echo "variable 1 : $VAR1"
+# echo "all vars : $@"
+# echo " no of vars : $#"
+# # how many varialbes passed
+
+
+#create a loop
+#for variable in list
+#do write your statements.
+
+for i in {1..100}
+do  
+    echo  $i
+done
