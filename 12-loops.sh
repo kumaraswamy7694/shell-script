1707e9b3abf3414dd7f2b601fa79fac145ce6d01
saik90305@gmail.com <saik90305@gmail.com>
2025-04-21 11:39:29 +0530

    added

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# DATE=$(date +%F)
-# LOGSDIR=/home/centos/shellscript-logs
-# SCRIPT_NAME=$(basename $0)
-# LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
-# USERID=$(id -u)
-# R="\e[31m"
-# N="\e[0m"
-# Y="\e[33m"
+DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
+SCRIPT_NAME=$(basename $0)
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+N="\e[0m"
+Y="\e[33m"
 
-# # Check if the script is run as root
-# if [ $USERID -ne 0 ]; then
-#     echo -e " $R error:: please run this script with root access $N"
-#     exit 1
-# fi 
+# Check if the script is run as root
+if [ $USERID -ne 0 ]; then
+    echo -e " $R error:: please run this script with root access $N"
+    exit 1
+fi 
 
-# # Validation function
-# VALIDATE() {
-#     if [ $1 -ne 0 ]; then
-#         echo -e " $R installing $2 .. FAILURE $N"
-#         exit 1
-#     else 
-#         echo -e " installing $2 .. $Y success $N"
-#     fi
-# }
+# Validation function
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e " $R installing $2 .. FAILURE $N"
+        exit 1
+    else 
+        echo -e " installing $2 .. $Y success $N"
+    fi
+}
 
 # Loop through the arguments
 for i in $@
