9ccd8dc4294f3fb69655038360cc70243d1bae63
saik90305@gmail.com <saik90305@gmail.com>
2025-04-21 11:21:30 +0530

    added

--- 09-functions.sh
+++ 09-functions.sh
@@ -3,14 +3,19 @@ DATE=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"  # Red
+G="\e[32m"  # Green
+N="\e[0m"   # Reset
+Y="\e[33m"  # Yellow
+LOGDIR=/tmp
 
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-        echo "$2 is failure"
+        echo "$2 $R failure $N"
         exit 1
     else
-        echo  -e "$2 is success"
+        echo  -e "$2 $G success $N"
     fi
 }
 USERID=$(id -u)
